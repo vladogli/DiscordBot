43ac66bcc3b0712c8bd5306c9f98b7d8360ba3a0
alleksh <allekshey@gmail.com>
2019-06-04 01:54:50 +0300

    Small fixes.

--- MainDir/BotPackages/TestPackage.py
+++ MainDir/BotPackages/TestPackage.py
@@ -1,61 +1,72 @@
 import sys
 import time
 import asyncio
+import os
+
+def getChannelLanguage(db, channelID):
+    a = db.SELECT("ChannelsAndLanguages",["language"], "channelID = " + str(channelID))
+    if len(a) == 0:
+        db.INSERT("ChannelsAndLanguages", [["language", "en-EN"], ["channelID", str(channelID)]])
+        return "en-EN"
+    return a[0][0]
+
 class Package:
     name = "TestPackage"
-    description = "That's just a test package."
     db = None
-    def __init__(self):
+    LocalisationReference = None
+
+    def __init__(self, LocalisationReference):
+        self.LocalisationReference = LocalisationReference
         print("Package initialized")
-    async def help(self, message, client):
-        if db is not None:
-            data = db.GetUserData(message.client.id)
+    
+    def getDescription(self, channelID):
+        return self.LocalisationReference.getText(self.name, "description", getChannelLanguage(self.db, channelID))
 
-
-            print(data)
-
-
-        await message.channel.send("this is help")
-
-
-
-    async def test_function(self, message, client):
-        if db is not None:
-            data = db.GetUserData(message.client.id)
-
-
-            print(data)
-
-
-        await message.channel.send("this is test")
-
-
-
-    async def updateFunction1(self):
-        while True:
-            print("updating....")
-            await asyncio.sleep(1)
-    async def updateFunction2(self):
-        while True:
-            print("updating2....")
-            await asyncio.sleep(1)
+    def getUpdateFunctions(self):
+        return None
+    
     def getCommands(self):
         return [
             ["help", self.help],
-            ["test", self.test_function]
+            ["test", self.test_function],
         ]
-    
-    def getUpdateFunctions(self):
-        return [
-            self.updateFunction1,
-            self.updateFunction2
-        ]
+
     def getAdditionalGuildValues(self):
-        return None
+        return [
+            ["TestPackageLanguage", "string"]
+        ]
+
     def getAdditionalUserValues(self):
         return [
             ["TestPackageHelpDelay", "int"],
             ["TestPackageTestFunctionDelay", "int"]
         ]
+
     def GiveDataBaseReference(self, db):
         self.db = db
+        try:
+            db.CREATE_TABLE("ChannelsAndLanguages", [["channelID", "string"], ["language", "string"]])
+        except:
+            pass
+
+    async def help(self, params, message, client):
+        if self.db is not None:
+            data = self.db.GetUserData(message.author.id)
+
+
+            print(data)
+
+
+        await message.channel.send(self.LocalisationReference.getText(self.name, "help", getChannelLanguage(self.db, message.channel.id)))
+
+
+
+    async def test_function(self, params, message, client):
+        if self.db is not None:
+            data = self.db.GetUserData(message.author.id)
+
+
+            print(data)
+
+
+        await message.channel.send(self.LocalisationReference.getText(self.name, "test", getChannelLanguage(self.db, message.channel.id)))
